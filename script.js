6eca57d9e14ded32edfd59a1ff290f5a418144e3
Mike Radcliffe <mikerad83@gmail.com>
2022-11-01 15:04:51 -0400

    Finished basic JS logic. Calculator computes for all operands, accepts numbers of varying length, allows for multiple consecutive operations, and blocks zero as a divisor.

--- script.js
+++ script.js
@@ -1,82 +1,184 @@
+// Declarations.
+
 const multiply = document.getElementById("multiply");
 const divide = document.getElementById("divide");
 const add = document.getElementById("plus");
 const subtract = document.getElementById("subtract");
 const equals = document.getElementById("equals");
 const clear = document.getElementById("clearButton");
-
+const display = document.getElementById("display");
 const buttons = document.querySelectorAll('button');
 
-let num1 = [];
-let num2 = [];
 let operand;
-
+let operandLiteral;
+let numTrue = false;
+let operandTrue = false;
+let equalsTrue = false;
 let input;
-let operandPush = false;
-
-let display = [];
-let displayCount = display.length - 1;
-let emptyDisplay = display.slice();
+let displayMemory = [];
 let calcNumbers = [];
-let calcNumCount = calcNumbers.length - 1;
+let calcNumber1 = [];
+let calcNumber2 = [];
 let currentNum = [];
-
-// for all buttons, i want to add an eventlistener tied to that button's ID
-// number inputs should build up in an array whose contents update in the display box in real time
-// those contents need to be saved as a number in a separate variable while display contains and shows
-// all digits entered for each number as well as any operands
+let makeNum;
 
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => 
-	    {
-	 	    input = button.id;
+	button.addEventListener('click', () => {
+        input = button.id;          
+                        
+		if (input === "multiply" || input === "divide" || input === "subtract" || input === "add") {
             
-            if(typeof input === "number") {
-                currentNum.push(input);
+            operand = input;            
 
-                display = emptyDisplay;
+            calcNumbers.push(makeNum);
+                console.log(`makeNum when operand is pushed: ${makeNum}`);
+                console.log(`calcNumbers when operand is pushed: ${calcNumbers}`);
 
-                for(i = 0; i < currentNum.length; i++) {
-                    display.push(currentNum[i]);
-                }
+            // if (an operand has already been pressed) {
+            //     -evaluate first two digits and display result
+            //     -display new operator following this result
+            // }
                 
-            } else if (input === "multiply" || input === "divide" || input === "subtract" || input === "add") {
-                let makeNum = Number(currentNum.join(""));
-                let newNum = currentNum.slice();
-                
-                operand = input;
-                display.push(operand);
-
-                calcNumbers.push[makeNum];
-                currentNum = newNum;
-
-            } else if (input === "equals") {
-                let result;
-
-                display = emptyDisplay;
-
-                for(i = 0; i < calcNumCount; i++) {
-                    result = calculate(calcNumbers[i], operand, calcNumbers(i + 1));
-
-                    display.push(result);
-                }
+            if (input === "multiply") {
+                operandLiteral = " * "
+            } else if (input === "divide") {
+            	operandLiteral = " / ";
+            } else if (input === "subtract") {
+            	operandLiteral = " - ";
+            } else if (input === "add") {
+            	operandLiteral = " + ";
             }
+
+            if (operandTrue === true) {
+                    console.log(operandTrue);
+                result = calculate(calcNumbers[0], operand, calcNumbers[1]);
+                if(calcNumbers.length > 1) {
+                    calcNumbers.shift();
+                }
+                calcNumbers.shift();
+                calcNumbers.unshift(result);
+                    console.log(`calcNumbers: ${calcNumbers}`);
+
+                let x = result.toString();
+                clearThis(displayMemory);
+                displayMemory.push(x);
+                displayMemory.push(operandLiteral);
+                display.textContent = displayMemory.join("");
+            
+            } else {
+                    console.log(operandTrue);
+                displayMemory.push(operandLiteral);
+                display.textContent = displayMemory.join("");
+
+                operandTrue = true;
+            
+            }
+
+            numTrue = false;
+
+            clearThis(currentNum);
+
+                console.log("**** END ****");
+
+        } else if (input === "equals") {
+            calcNumbers.push(makeNum);
+
+            let result;
+            let calcNumCount = calcNumbers.length - 1;
+
+            equalsTrue = true;
+
+            if (operandTrue === false) {
+                displayMemory.push(` = ${makeNum}`);
+                display.textContent = displayMemory.join("");
+
+            } else {    
+                operandTrue = false;
+
+            
+            
+            displayMemory.push(" = ");
+
+                console.log(`This is what calcNumbers looks like before running calculate(): ${calcNumbers}`);
+
+            for (i = 0; i < calcNumCount; i++) {
+                result = calculate(calcNumbers[0], operand, calcNumbers[1]);
+                    console.log(`Result of 'calculate': ${result}`);
+                if (i > 0) {
+                    calcNumbers.shift();
+                }
+                calcNumbers.shift();
+                calcNumbers.unshift(result);
+                    console.log(`calcNumbers: ${calcNumbers}`);
+            }
+
+            let x = result.toString();
+            displayMemory.push(x);
+            display.textContent = displayMemory.join("");
+            
+            }
+
+            clearThis(calcNumbers);
+            clearThis(currentNum);
+
+        } else if (input === "clearButton") {
+            clearThis(displayMemory);
+                console.log(displayMemory);
+            clearThis(calcNumbers);
+            clearThis(currentNum);
+
+            display.textContent = displayMemory;
+
+        } else {
+            if(equalsTrue) {
+                clearThis(displayMemory);
+                equalsTrue = false;
+            }
+
+		    input = Number(button.id);
+               	console.log(`This is the initial value for displayMemory: ${displayMemory}`);
+               	console.log(`This is how many numbers are currently in displayMemory: ${displayMemory.length}`);
+                console.log(`This is the initial value of currentNum: ${currentNum}`);
+
+            currentNum.push(input);
+                console.log(`This is currentNum after pushing the input value: ${currentNum}`);
+
+            let a = currentNum.join("");
+            makeNum = Number(a);
+                console.log(`makeNum: ${makeNum}`);
+
+            if(numTrue === true) {
+                displayMemory.pop();
+
+            }
+
+            displayMemory.push(currentNum.join(""));                    
+               	console.log(`This is the value of displayMemory after pushing the updated currentNum: ${displayMemory}`);
+            display.textContent = displayMemory.join("");
+
+            numTrue = true;
+
+                console.log("**** END ****");                    
+
+		    }
         });
 });
 
+
+// These are the functions utilized by the code.
 
 function calculate(x, y, z) {
     let result;
 
     if (y === "add") {
-        result = add(x, z);
+        result = addition(x, z);
     } else if (y === "subtract") {
-        result = subtract(x, z);
+        result = subtraction(x, z);
     } else if (y === "multiply") {
-        result = multiply(x, z);
+        result = multiplication(x, z);
     } else if (y === "divide") {
-        result = divide(x, z);
+        result = division(x, z);
     } else {
         result = "ERROR";
     }
@@ -84,18 +186,26 @@ function calculate(x, y, z) {
     return result;
 }
 
-function add(x, y) {
+function addition(x, y) {
     return x + y;
 }
 
-function subtract(x, y) {
+function subtraction(x, y) {
     return x - y;
 }
 
-function mulitply(x, y) {
+function multiplication(x, y) {
     return x * y;
 }
 
-function divide(x, y) {
-    return x / y;
+function division(x, y) {
+    if (y === 0) {
+        return "If you want it to divide, then ante up. If you want to play it cheap, be on welfare the whole winter. I don't want no volunteers, I don't want no mates, there's just too many captains on this island. A non-zero divisor for me by myself. For that you get the head, the tail, the whole damn thing.";
+    } else {
+        return x / y;
+    }
+}
+
+function clearThis(x) {
+    x.splice(0, x.length);
 }
